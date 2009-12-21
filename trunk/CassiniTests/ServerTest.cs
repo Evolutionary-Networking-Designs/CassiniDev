2356d2df56cb68304f8719ebe6864c876f681afd
SND\Sky_cp <SND\Sky_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2009-12-21 15:19:48 +0000

    Added some constructors to Server Added reuseConnection option Added support for Fiddler2 in Server.RootUrl
    git-svn-id: https://cassinidev.svn.codeplex.com/svn@33160 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ trunk/CassiniTests/ServerTest.cs
@@ -0,0 +1,95 @@
+﻿// /* **********************************************************************************
+//  *
+//  * Copyright (c) Sky Sanders. All rights reserved.
+//  * 
+//  * This source code is subject to terms and conditions of the Microsoft Public
+//  * License (Ms-PL). A copy of the license can be found in the license.htm file
+//  * included in this distribution.
+//  *
+//  * You must not remove this notice, or any other, from this software.
+//  *
+//  * **********************************************************************************/
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+
+#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+
+#endregion
+
+#endif
+
+namespace CassiniTests
+{
+    /// <summary>
+    ///This is a test class for ServerTest and is intended
+    ///to contain all ServerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ServerTest
+    {
+        #region Setup/Teardown
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+        }
+
+        #endregion
+
+        public static int GetAvailablePort(int startingPort, int endingPoint, bool includeTimeWaitPorts)
+        {
+            IPEndPoint[] endPoints;
+            List<int> portArray = new List<int>();
+
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+
+            //getting active connections
+            TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
+            portArray.AddRange(from n in connections
+                               where n.LocalEndPoint.Port >= startingPort && n.LocalEndPoint.Port <= endingPoint &&
+                                     (includeTimeWaitPorts || (n.State != TcpState.TimeWait))
+                               select n.LocalEndPoint.Port);
+
+            //getting active tcp listners - WCF service listening in tcp
+            endPoints = properties.GetActiveTcpListeners();
+            portArray.AddRange(from n in endPoints
+                               where n.Port >= startingPort
+                               select n.Port);
+
+            //getting active udp listeners
+            endPoints = properties.GetActiveUdpListeners();
+            portArray.AddRange(from n in endPoints
+                               where n.Port >= startingPort
+                               select n.Port);
+
+            portArray.Sort();
+
+            for (int i = startingPort; i < UInt16.MaxValue; i++)
+                if (!portArray.Contains(i))
+                    return i;
+
+            return 0;
+        }
+
+        [TestMethod]
+        public void TestServerReuseConnection()
+        {
+        }
+    }
+}
--- trunk/cassini-v35/Server.cs
+++ trunk/cassini-v35/Server.cs
@@ -12,6 +12,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Net;
@@ -23,57 +24,96 @@ using System.Threading;
 using System.Web;
 using System.Web.Hosting;
 
-namespace Cassini {
+namespace Cassini
+{
     /// <summary>
     /// 
     /// </summary>
     /// <changes>
-    /// 12/20/09 - changed visibility from internal to public
+    /// 12/20/09 - sky: changed visibility from internal to public
+    /// 12/21/09 - sky: * added IDisposable implementation
+    ///                 * set socketoptions to reuse time_wait sockets enabling if flag is set- 
+    ///                 rapid buildup and teardown of server instances for testing purposes
+    ///                 * modified socket creation to throw if both v4 and v6 fail.
+    ///                 * added constructors 
+    ///                 * changed CreateSocketBindAndListen to instance method to allow using instance flag
+    /// 
+    /// TODO: 
+    ///   * allow specifying specific IP or any IP.
+    ///   
+    /// 
     /// </changes>
-    public class Server : MarshalByRefObject {
+    public class Server : MarshalByRefObject, IDisposable
+    {
         int _port;
         string _virtualPath;
         string _physicalPath;
+        private bool _reuseConnection;
         bool _shutdownInProgress;
         Socket _socket;
         Host _host;
+        public Server()
+        {
 
-        public Server(int port, string virtualPath, string physicalPath) {
+        }
+        public Server(int port, string virtualPath, string physicalPath, bool reuseConnection)
+        {
             _port = port;
             _virtualPath = virtualPath;
             _physicalPath = physicalPath.EndsWith("\\", StringComparison.Ordinal) ? physicalPath : physicalPath + "\\";
+            _reuseConnection = reuseConnection;
         }
 
-        public override object InitializeLifetimeService() {
+        public Server(int port, string virtualPath, string physicalPath) : this(port, virtualPath, physicalPath, false)
+        {
+
+        }
+
+        public override object InitializeLifetimeService()
+        {
             // never expire the license
             return null;
         }
 
-        public string VirtualPath {
-            get {
+        public string VirtualPath
+        {
+            get
+            {
                 return _virtualPath;
             }
         }
 
-        public string PhysicalPath {
-            get {
+        public string PhysicalPath
+        {
+            get
+            {
                 return _physicalPath;
             }
         }
 
-        public int Port {
-            get {
+        public int Port
+        {
+            get
+            {
                 return _port;
             }
         }
 
-        public string RootUrl {
-            get {
-                if (_port != 80) {
-                    return "http://localhost:" + _port + _virtualPath;
+        /// <summary>
+        /// 12/21/09 sky: * changed localhost to 127.0.0.1 and added '.' to localhost to enable fiddler to pick up traffic
+        ///               see http://www.west-wind.com/weblog/posts/596348.aspx
+        /// </summary>
+        public string RootUrl
+        {
+            get
+            {
+                if (_port != 80)
+                {
+                    return "http://127.0.0.1.:" + _port + _virtualPath;
                 }
-                else {
-                    return "http://localhost" + _virtualPath;
+                else
+                {
+                    return "http://127.0.0.1." + _virtualPath;
                 }
             }
         }
@@ -82,39 +122,74 @@ namespace Cassini {
         // Socket listening
         // 
 
-        static Socket CreateSocketBindAndListen(AddressFamily family, IPAddress address, int port) {
+        private Socket CreateSocketBindAndListen(AddressFamily family, IPAddress address, int port)
+        {
             var socket = new Socket(family, SocketType.Stream, ProtocolType.Tcp);
+            //SKY: in a testing scenario we may be creating and disposing many servers in rapid
+            // succession. Default socket behaviour will send the closed socket into the TIME_WAIT
+            // state, a sort of cooldown period, making it unavaible. By setting ReuseAddress we are telling winsock to accept the
+            // binding event if the socket is in TIME_WAIT. 
+            if(_reuseConnection)
+            {
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            }
+            
             socket.Bind(new IPEndPoint(address, port));
             socket.Listen((int)SocketOptionName.MaxConnections);
             return socket;
         }
 
-        public void Start() {
-            try {
+        public void Start()
+        {
+
+
+
+            try
+            {
                 _socket = CreateSocketBindAndListen(AddressFamily.InterNetwork, IPAddress.Loopback, _port);
+                Trace.WriteLine(string.Format("OPENED:{0}:{1}", IPAddress.Loopback, _port));
             }
-            catch {
-                _socket = CreateSocketBindAndListen(AddressFamily.InterNetworkV6, IPAddress.IPv6Loopback, _port);
+            catch (Exception ex)
+            {
+
+                try
+                {
+                    _socket = CreateSocketBindAndListen(AddressFamily.InterNetworkV6, IPAddress.IPv6Loopback, _port);
+                    Trace.WriteLine(string.Format("OPENED:{0}:{1}", IPAddress.Loopback, _port));
+                }
+                catch (Exception ex2)
+                {
+                    Trace.WriteLine(string.Format("ERROR:{0}:{1} - \nv4:{2}\nv6:{3}", IPAddress.Loopback, _port, ex.Message, ex2.Message));
+                    throw;
+                }
+
             }
 
-            ThreadPool.QueueUserWorkItem(delegate {
-                while (!_shutdownInProgress) {
-                    try {
+            ThreadPool.QueueUserWorkItem(delegate
+            {
+                while (!_shutdownInProgress)
+                {
+                    try
+                    {
                         Socket acceptedSocket = _socket.Accept();
 
-                        ThreadPool.QueueUserWorkItem(delegate {
-                            if (!_shutdownInProgress) {
+                        ThreadPool.QueueUserWorkItem(delegate
+                        {
+                            if (!_shutdownInProgress)
+                            {
                                 var conn = new Connection(this, acceptedSocket);
 
                                 // wait for at least some input
-                                if (conn.WaitForRequestBytes() == 0) {
+                                if (conn.WaitForRequestBytes() == 0)
+                                {
                                     conn.WriteErrorAndClose(400);
                                     return;
                                 }
 
                                 // find or create host
                                 Host host = GetHost();
-                                if (host == null) {
+                                if (host == null)
+                                {
                                     conn.WriteErrorAndClose(500);
                                     return;
                                 }
@@ -124,39 +199,51 @@ namespace Cassini {
                             }
                         });
                     }
-                    catch {
+                    catch
+                    {
                         Thread.Sleep(100);
                     }
                 }
             });
         }
 
-        public void Stop() {
+        public void Stop()
+        {
             _shutdownInProgress = true;
 
-            try {
-                if (_socket != null) {
+            try
+            {
+                if (_socket != null)
+                {
                     _socket.Close();
                 }
             }
-            catch {
+            catch
+            {
             }
-            finally {
+            finally
+            {
                 _socket = null;
+                Thread.Sleep(0);
             }
 
-            try {
-                if (_host != null) {
+            try
+            {
+                if (_host != null)
+                {
                     _host.Shutdown();
                 }
 
-                while (_host != null) {
+                while (_host != null)
+                {
                     Thread.Sleep(100);
                 }
             }
-            catch {
+            catch
+            {
             }
-            finally {
+            finally
+            {
                 _host = null;
             }
         }
@@ -164,24 +251,30 @@ namespace Cassini {
         // called at the end of request processing
         // to disconnect the remoting proxy for Connection object
         // and allow GC to pick it up
-        public void OnRequestEnd(Connection conn) {
+        public void OnRequestEnd(Connection conn)
+        {
             RemotingServices.Disconnect(conn);
         }
 
-        public void HostStopped() {
+        public void HostStopped()
+        {
             _host = null;
         }
 
-        Host GetHost() {
+        Host GetHost()
+        {
             if (_shutdownInProgress)
                 return null;
 
             Host host = _host;
 
-            if (host == null) {
-                lock (this) {
+            if (host == null)
+            {
+                lock (this)
+                {
                     host = _host;
-                    if (host == null) {
+                    if (host == null)
+                    {
                         host = (Host)CreateWorkerAppDomainWithHost(_virtualPath, _physicalPath, typeof(Host));
                         host.Configure(this, _port, _virtualPath, _physicalPath);
                         _host = host;
@@ -192,7 +285,8 @@ namespace Cassini {
             return host;
         }
 
-        static object CreateWorkerAppDomainWithHost(string virtualPath, string physicalPath, Type hostType) {
+        static object CreateWorkerAppDomainWithHost(string virtualPath, string physicalPath, Type hostType)
+        {
             // this creates worker app domain in a way that host doesn't need to be in GAC or bin
             // using BuildManagerHost via private reflection
             string uniqueAppString = string.Concat(virtualPath, physicalPath).ToLowerInvariant();
@@ -213,6 +307,31 @@ namespace Cassini {
 
             // create Host in the worker app domain
             return appManager.CreateObject(appId, hostType, virtualPath, physicalPath, false);
+        }
+
+
+        // implement disposable to help eliminate zombie ports
+        private bool _disposed;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    Stop();
+                }
+            }
+            _disposed = true;
+        }
+
+        ~Server()
+        {
+            Dispose(false);
         }
     }
 }
