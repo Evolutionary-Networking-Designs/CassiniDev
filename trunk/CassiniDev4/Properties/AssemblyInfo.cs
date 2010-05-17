999d9423e08df6a7b65298f6faa042dd554ae1e5
SND\Sky_cp <SND\Sky_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-05-17 00:30:28 +0000

    4.0.1.0 beta
    git-svn-id: https://cassinidev.svn.codeplex.com/svn@49847 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ trunk/CassiniDev4/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CassiniDev")]
+[assembly: AssemblyDescription("Cassini For Developers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Salient Solutions")]
+[assembly: AssemblyProduct("CassiniDev")]
+[assembly: AssemblyCopyright("Copyright © Sky Sanders. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: ComVisible(false)]
