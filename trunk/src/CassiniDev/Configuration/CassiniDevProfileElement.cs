f3bd67fa21eea5cc847555235f6454af221b3a5b
SND\Sky_cp <SND\Sky_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-07-08 18:18:04 +0000

    interim checkin - working on CI - not ready for public
    git-svn-id: https://cassinidev.svn.codeplex.com/svn@92560 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ trunk/src/CassiniDev/Configuration/CassiniDevProfileElement.cs
@@ -0,0 +1,115 @@
+﻿using System.Configuration;
+
+namespace CassiniDev.Configuration
+{
+    ///<summary>
+    ///</summary>
+    public class CassiniDevProfileElement : ConfigurationElement
+    {
+        /// <summary>
+        /// Port is used as profile selector
+        /// </summary>
+        [ConfigurationProperty("port", DefaultValue = "*", IsKey = true, IsRequired = true)]
+        public string Port
+        {
+            get
+            {
+                return (string)this["port"];
+            }
+            set
+            {
+                this["port"] = value;
+            }
+        }
+
+        ///<summary>
+        ///</summary>
+        [ConfigurationProperty("path")]
+        public string Path
+        {
+            get
+            {
+                return (string)this["path"];
+            }
+            set
+            {
+                this["path"] = value;
+            }
+        }
+
+
+
+
+        ///<summary>
+        ///</summary>
+        [ConfigurationProperty("hostName")]
+        public string HostName
+        {
+            get
+            {
+                return (string)this["hostName"];
+            }
+            set
+            {
+                this["hostName"] = value;
+            }
+        }
+
+        ///<summary>
+        ///</summary>
+        [ConfigurationProperty("ip")]
+        public string IpAddress
+        {
+            get
+            {
+                return (string)this["ip"];
+            }
+            set
+            {
+                this["ip"] = value;
+            }
+        }
+
+        ///<summary>
+        ///</summary>
+        [ConfigurationProperty("ipMode", DefaultValue = CassiniDev.IPMode.Loopback)]
+        public IPMode IpMode
+        {
+            get
+            {
+                return (IPMode)this["ipMode"];
+            }
+            set
+            {
+                this["ipMode"] = value;
+            }
+        }
+
+        ///<summary>
+        ///</summary>
+        [ConfigurationProperty("v6", DefaultValue = false)]
+        public bool IpV6
+        {
+            get
+            {
+                return (bool)this["v6"];
+            }
+            set
+            {
+                this["v6"] = value;
+            }
+        }
+
+
+        ///<summary>
+        ///</summary>
+        [ConfigurationProperty("plugins")]
+        public PluginElementCollection Plugins
+        {
+            get
+            {
+                return (PluginElementCollection)this["plugins"];
+            }
+        }
+    }
+}
